8a659d55529918012234e662413a3758a2ba3db1
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2024-03-26 14:12:09 -0400

    Initial implementation (#1)

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
+[![project chat](https://img.shields.io/badge/zulip-join_chat-brightgreen.svg)](https://nixos.zulipchat.com/#narrow/stream/413949-haskell-flake)
+
 # colmena-flake
-A `flake-parts` module for colmena deployment tool
+
+A `flake-parts` module for [colmena](https://github.com/zhaofengli/colmena) deployment tool
+
+## Why
+
+- Re-use an existing `nixosConfigurations`
+
+## How to use
+
+1. Keep your existing NixOS configuration in `nixosConfigurations.<name>` (business as usual)
+2. Add your [colmena deployment options](https://colmena.cli.rs/unstable/reference/deployment.html) for any of those configurations, decoupled, under `flake.colmena-parts.deployment.<name>`.
+3. Add `pkgs.colmena` to your devShell, and run `colmena apply` to deploy.
---
+++ flake-module.nix
@@ -0,0 +1,61 @@
+{ config, lib, self, inputs, ... }:
+
+{
+  options = {
+    colmena-parts = lib.mkOption {
+      default = { };
+      type = lib.types.submodule {
+        options = {
+          deployment = lib.mkOption {
+            type = lib.types.attrsOf (lib.types.attrsOf lib.types.raw);
+            description = ''
+              Deployment configuration for colmena nodes
+            '';
+          };
+
+          system = lib.mkOption {
+            type = lib.types.str;
+            description = ''
+              The system for colmena nodes
+            '';
+            default = "x86_64-linux";
+          };
+
+          sshConn = lib.mkOption {
+            type = lib.types.attrsOf lib.types.str;
+            description = ''
+              The SSH connection string for colmena nodes
+            '';
+            default = builtins.mapAttrs (_: v: "${v.targetUser}@${v.targetHost}") config.colmena-parts.deployment;
+            readOnly = true;
+          };
+        };
+      };
+    };
+  };
+
+  config.flake = {
+    colmena-parts = {
+      inherit (config.colmena-parts) sshConn;
+    };
+
+    colmena = {
+      meta = {
+        nixpkgs = import inputs.nixpkgs {
+          inherit (config.colmena-parts) system;
+          overlays = [ ];
+        };
+        # https://github.com/zhaofengli/colmena/issues/60#issuecomment-1510496861
+        nodeSpecialArgs = builtins.mapAttrs (_: value: value._module.specialArgs) self.nixosConfigurations;
+      };
+    } // builtins.mapAttrs
+      (name: value: {
+        imports = value._module.args.modules ++ [{
+          deployment = config.colmena-parts.deployment.${name};
+        }];
+      })
+      (lib.filterAttrs
+        (k: _: lib.hasAttr k config.colmena-parts.deployment)
+        self.nixosConfigurations);
+  };
+}
